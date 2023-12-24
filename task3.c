089ce1d466e724324045fabd24263d43103fefac
Kelvin Okoye <okoyekelvin347@gmail.com>
2023-12-24 16:20:45 +0100

    task3

---
+++ task3.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+
+/*print a pattern 
+    1,
+    1,2
+    1,2,3
+    1,2,3,4
+    1,2,3,4,5
+till n.
+*/
+
+
+int main(void)
+{
+    int num, i, j;
+    printf("Enter a Number: ");
+    scanf("%d", &num);
+
+    for (i = 1; i <= num; i++)
+    {
+        /* code */
+
+        for (j = 1; j <= i; j++)
+        {
+            /* code */
+
+            printf("%d", j);
+        }
+        
+        printf("\n");
+    }
+
+    return 0;
+}
